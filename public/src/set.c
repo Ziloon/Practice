4e4ebe5486375f30f0e8e8fe3f1a2f04573bd01a
赵志龙 <ziloon@zhaozhilongdeMacBook-Pro.local>
2019-03-17 23:51:26 +0800

    格式化

--- public/src/set.c
+++ public/src/set.c
@@ -1,14 +1,13 @@
 #include "set.h"
-#include "log.h"
-#include "type.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "log.h"
+#include "type.h"
 
 LOG_IMPORT("Set", LOG_LEV_INFO);
 
-int set_test()
-{
+int set_test() {
     int i = 0;
     int e = 0;
     int max_len = 0;
@@ -18,23 +17,20 @@ int set_test()
 
     logInfo("please input max len of set:\t");
 
-    if (1 != scanf("%d", &max_len))
-    {
+    if (1 != scanf("%d", &max_len)) {
         return -1;
     }
 
     init(&stS, max_len);
     display(stS);
 
-    for (i = max_len; i > 0; i--)
-    {
+    for (i = max_len; i > 0; i--) {
         append(&stS, i);
     }
     display(stS);
 
     logInfo("input remove element:\t");
-    if (1 != scanf("%d", &e))
-    {
+    if (1 != scanf("%d", &e)) {
         return -1;
     }
 
@@ -42,8 +38,7 @@ int set_test()
     display(stS);
 
     logInfo("input remove element:\t");
-    if (1 != scanf("%d", &e))
-    {
+    if (1 != scanf("%d", &e)) {
         return -1;
     }
 
@@ -62,52 +57,40 @@ int set_test()
     return 0;
 }
 
-int display(set s)
-{
+int display(set s) {
     int i = 0;
     printf("(%p): len %d, max len %d\n", &s, s.len, s.max_len);
 
-    if (NULL != s.data)
-    {
+    if (NULL != s.data) {
         printf("content(%p): \n", s.data);
-        for (i = 0; i < s.max_len; ++i)
-        {
-            if (FALSE == s._flag[i])
-            {
+        for (i = 0; i < s.max_len; ++i) {
+            if (FALSE == s._flag[i]) {
                 printf("[%2d]%-8s ", i, "");
-            }
-            else
-            {
+            } else {
                 printf("[%2d]%-8d ", i, s.data[i]);
             }
 
-            if (3 == i % 4)
-            {
+            if (3 == i % 4) {
                 printf("\n");
             }
         }
         printf("\n");
-    }
-    else
-    {
+    } else {
         printf("content null\n");
     }
 
     return 0;
 }
 
-int init(set *s, unsigned max_len)
-{
-    if (NULL != s->data)
-    {
+int init(set *s, unsigned max_len) {
+    if (NULL != s->data) {
         logInfo("s data(%p)is already init, re-build.\r\n", s->data);
         destory(s);
     }
 
     s->data = (int *)malloc(sizeof(int) * max_len);
     s->_flag = (BOOL *)malloc(sizeof(BOOL) * max_len);
-    if (NULL == s->data)
-    {
+    if (NULL == s->data) {
         logErr("s malloc data space fail.\r\n");
         return -1;
     }
@@ -120,10 +103,8 @@ int init(set *s, unsigned max_len)
     return 0;
 }
 
-int destory(set *s)
-{
-    if (NULL != s->data)
-    {
+int destory(set *s) {
+    if (NULL != s->data) {
         free(s->data);
         s->data = NULL;
     }
@@ -134,56 +115,41 @@ int destory(set *s)
     return 0;
 }
 
-BOOL is_full(set s)
-{
-    if (s.len == s.max_len)
-    {
+BOOL is_full(set s) {
+    if (s.len == s.max_len) {
         return TRUE;
-    }
-    else
-    {
+    } else {
         return FALSE;
     }
 }
 
-BOOL is_empty(set s)
-{
-    if (0 == s.len)
-    {
+BOOL is_empty(set s) {
+    if (0 == s.len) {
         return TRUE;
-    }
-    else
-    {
+    } else {
         return FALSE;
     }
 }
 
-int find(set s, int e)
-{
-    for (int i = 0; i < s.max_len; ++i)
-    {
-        if (e == s.data[i])
-        {
+int find(set s, int e) {
+    for (int i = 0; i < s.max_len; ++i) {
+        if (e == s.data[i]) {
             return i;
         }
     }
     return -1;
 }
 
-int append(set *s, int e)
-{
+int append(set *s, int e) {
     int i = 0;
 
-    if (TRUE == is_full(*s))
-    {
+    if (TRUE == is_full(*s)) {
         logErr("full set\n");
         return -1;
     }
 
-    for (i = 0; i < s->max_len; ++i)
-    {
-        if (FALSE == s->_flag[i])
-        {
+    for (i = 0; i < s->max_len; ++i) {
+        if (FALSE == s->_flag[i]) {
             s->data[i] = e;
             s->_flag[i] = TRUE;
             s->len += 1;
@@ -194,18 +160,15 @@ int append(set *s, int e)
     return -1;
 }
 
-int set_remove(set *s, int e)
-{
+int set_remove(set *s, int e) {
     int i = -1;
 
-    if (TRUE == is_empty(*s))
-    {
+    if (TRUE == is_empty(*s)) {
         return i;
     }
 
     i = find(*s, e);
-    if (i >= 0)
-    {
+    if (i >= 0) {
         s->data[i] = 0;
         s->_flag[i] = FALSE;
         s->len -= 1;
